99aa462668357c91cd484eee6e1dcd2ee82eddc7
iamjiangshui <781114941@qq.com>
2018-05-30 18:46:11 +0800

    init

---
+++ src/sms/main.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iamjiangshui
+ * Date: 2018/5/30
+ * Time: 18:42
+ */
+namespace src\sms;
+class smsConfig {
+    public function index() {
+        echo 'jj';
+    }
+}
