8e2b8fa0ccaed945071addd9fee4b2d9e6c80652
iamjiangshui <781114941@qq.com>
2018-05-31 12:31:42 +0800

    update

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 
 require 'vendor/autoload.php';
-$arr = [1,2,3];
-$sms = new JSsoft\smsSdk\smsConfig($arr);
-$sms->index();
-$sms->test();
+$arr = array('accessKeyId'=>'LTAIC6lV6wHtZxmF', 'accessKeySecret'=>'qrGwrLtwBhKZr5MZstjAg5J5ibPQxf');
+$sms = new JSsoft\smsSdk\alidayu($arr);
+$info = array('phoneNumbers'=>array('15196378118'),
+'signName'=>array('科技与创新部'),
+'templateCode'=>'SMS_132401546',
+'data'=>array(array('code'=>'5555'))
+);
+$res = $sms->querySendDetails();
+echo json_encode(object_array($res));
+function object_array($array)
+{
+   if(is_object($array))
+   {
+    $array = (array)$array;
+   }
+   if(is_array($array))
+   {
+    foreach($array as $key=>$value)
+    {
+     $array[$key] = object_array($value);
+    }
+   }
+   return $array;
+}
